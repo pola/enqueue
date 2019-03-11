74ecd0ba22b7fbba922df01a782146dd864ca27a
Martin Pola <mpola@kth.se>
2019-03-11 15:31:09 +0100

    tog bort spårutskrifter

--- app/controllers/socket.controller.js
+++ app/controllers/socket.controller.js
@@ -5,24 +5,20 @@ module.exports = (socket, io) =>
 	socket.on('broadcast', data => {
 		console.log(data);
 		if (!('queue' in data) || !('message' in data) || typeof data.queue !== 'number' || typeof data.message !== 'string') {
-			console.log('1');
 			return;
 		}
 		
 		if (!('profile' in socket.handshake.session)) {
-			console.log('2');
 			return;
 		}
 		
 		model.get_queue(data.queue).then(queue => {
 			if (queue === null) {
-				console.log('3');
 				return;
 			}
 			
 			model.has_permission(queue, socket.handshake.session.profile.id).then(has_permission => {
 				if (!has_permission) {
-					console.log('4');
 					return;
 				}
 				
