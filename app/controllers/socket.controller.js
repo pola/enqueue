b63e8b66f005a00646052dde70457ece65043f7e
Martin Pola <mpola@kth.se>
2019-03-11 16:17:52 +0100

    felhantering m.m. vid broadcast och notify

--- app/controllers/socket.controller.js
+++ app/controllers/socket.controller.js
@@ -5,24 +5,54 @@ module.exports = (socket, io) =>
 	socket.on('broadcast', data => {
 		console.log(data);
 		if (!('queue' in data) || !('message' in data)) {
+			socket.emit('broadcast_status', {
+				success: false,
+				queue: null,
+				error: 'INVALID_PARAMETERS',
+				message: 'Invalid or missing parameters.'
+			});
 			return;
 		}
 		
 		if (typeof data.queue !== 'number' || typeof data.message !== 'string') {
+			socket.emit('broadcast_status', {
+				success: false,
+				queue: null,
+				error: 'INVALID_PARAMETERS',
+				message: 'Invalid or missing parameters.'
+			});
 			return;
 		}
 		
 		if (!('profile' in socket.handshake.session)) {
+			socket.emit('broadcast_status', {
+				success: false,
+				queue: null,
+				error: 'PERMISSION_DENIED',
+				message: 'You do not have permission to sent notifications.'
+			});
 			return;
 		}
 		
 		model.get_queue(data.queue).then(queue => {
 			if (queue === null) {
+				socket.emit('notify_status', {
+					success: false,
+					queue: null,
+					error: 'UNKNOWN_QUEUE',
+					message: 'The specified queue was not found.'
+				});
 				return;
 			}
 			
 			model.has_permission(queue, socket.handshake.session.profile.id).then(has_permission => {
 				if (!has_permission) {
+					socket.emit('broadcast_status', {
+						success: false,
+						queue: queue.id,
+						error: 'PERMISSION_DENIED',
+						message: 'You do not have permission to sent notifications.'
+					});
 					return;
 				}
 				
@@ -37,36 +67,78 @@ module.exports = (socket, io) =>
 					message: data.message,
 					sender: profile
 				});
+				
+				socket.emit('broadcast_status', {
+					success: true,
+					queue: queue.id,
+					message: data.message
+				});
 			});
 		});
 	});
 	
 	socket.on('notify', data => {
 		if (!('queue' in data) || !('message' in data) || !('recipient' in data)) {
+			socket.emit('notify_status', {
+				success: false,
+				queue: null,
+				error: 'INVALID_PARAMETERS',
+				message: 'Invalid or missing parameters.'
+			});
 			return;
 		}
 		
 		if (typeof data.queue !== 'number' || typeof data.message !== 'string' || typeof data.recipient !== 'string') {
+			socket.emit('notify_status', {
+				success: false,
+				queue: null,
+				error: 'INVALID_PARAMETERS',
+				message: 'Invalid or missing parameters.'
+			});
 			return;
 		}
 		
 		if (!('profile' in socket.handshake.session)) {
+			socket.emit('notify_status', {
+				success: false,
+				queue: null,
+				error: 'PERMISSION_DENIED',
+				message: 'You do not have permission to sent notifications.'
+			});
 			return;
 		}
 		
 		model.get_queue(data.queue).then(queue => {
 			if (queue === null) {
+				socket.emit('notify_status', {
+					success: false,
+					queue: null,
+					error: 'UNKNOWN_QUEUE',
+					message: 'The specified queue was not found.'
+				});
 				return;
 			}
 			
 			const queuing_student = model.get_queuing(queue).find(x => x.profile.id === data.recipient);
 			
 			if (queuing_student === undefined) {
+				socket.emit('notify_status', {
+					success: false,
+					queue: queue.id,
+					error: 'UNKNOWN_QUEING_STUDENT',
+					message: 'The specified student is not in the queue.'
+				});
 				return;
 			}
 			
 			model.has_permission(queue, socket.handshake.session.profile.id).then(has_permission => {
 				if (!has_permission) {
+					socket.emit('notify_status', {
+						success: false,
+						queue: queue.id,
+						error: 'PERMISSION_DENIED',
+						message: 'You do not have permission to sent notifications.'
+					});
 					return;
 				}
 				
@@ -80,7 +152,6 @@ module.exports = (socket, io) =>
 					const socket = io.sockets.sockets[k];
 					
 					if ('profile' in socket.handshake.session && socket.handshake.session.profile.id === queuing_student.profile.id) {
-						// TODO: skicka bara till den det berör, queuing_student.profile.id
 						socket.emit('notify', {
 							queue: queue.id,
 							message: data.message,
@@ -88,6 +159,13 @@ module.exports = (socket, io) =>
 						});
 					}
 				}
+				
+				socket.emit('notify_status', {
+					success: true,
+					queue: queue.id,
+					recipient: queuing_student,
+					message: data.message
+				});
 			});
 		});
 	});
